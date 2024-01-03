6e2a0876f27d0e991f325d0c443e2f1fe1765216
Sebastian Flügge <seflue@web.de>
2024-01-03 01:19:59 +0100

    chore: reformat project with stylua

--- lua/telescope/_extensions/orgmode/refile_heading.lua
+++ lua/telescope/_extensions/orgmode/refile_heading.lua
@@ -1,10 +1,10 @@
-local pickers = require("telescope.pickers")
-local finders = require("telescope.finders")
-local conf = require("telescope.config").values
-local action_set = require("telescope.actions.set")
-local actions = require("telescope.actions")
-local action_state = require("telescope.actions.state")
-local state = require("telescope.state")
+local pickers = require('telescope.pickers')
+local finders = require('telescope.finders')
+local conf = require('telescope.config').values
+local action_set = require('telescope.actions.set')
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+local state = require('telescope.state')
 
 local utils = require('telescope-orgmode.utils')
 
@@ -59,7 +59,6 @@ return function(opts)
   end
 
   local function gen_depth_toggle(opts, prompt_bufnr)
-
     local status = state.get_status(prompt_bufnr)
     status._ot_current_depth = opts.max_depth
     status._ot_next_depth = nil
@@ -75,29 +74,31 @@ return function(opts)
       status._ot_next_depth = aux
 
       opts.max_depth = status._ot_current_depth
-      local new_finder = finders.new_table {
+      local new_finder = finders.new_table({
         results = utils.get_entries(opts),
         entry_maker = opts.entry_maker or utils.make_entry(opts),
-      }
+      })
 
       current_picker:refresh(new_finder, opts)
     end
   end
 
-  pickers.new(opts, {
-     -- TODO: alter prompt title when depth is 0: Refile under file, Refile
-     -- under Headline
-    prompt_title = "Refile Destination",
-    finder = finders.new_table {
-      results = utils.get_entries(opts),
-      entry_maker = opts.entry_maker or utils.make_entry(opts),
-    },
-    sorter = conf.generic_sorter(opts),
-    previewer = conf.grep_previewer(opts),
-    attach_mappings = function(prompt_bufnr, map)
-      action_set.select:replace(refile)
-      map("i", "<c-space>", gen_depth_toggle(opts, prompt_bufnr))
-      return true
-    end,
-  }):find()
+  pickers
+    .new(opts, {
+      -- TODO: alter prompt title when depth is 0: Refile under file, Refile
+      -- under Headline
+      prompt_title = 'Refile Destination',
+      finder = finders.new_table({
+        results = utils.get_entries(opts),
+        entry_maker = opts.entry_maker or utils.make_entry(opts),
+      }),
+      sorter = conf.generic_sorter(opts),
+      previewer = conf.grep_previewer(opts),
+      attach_mappings = function(prompt_bufnr, map)
+        action_set.select:replace(refile)
+        map('i', '<c-space>', gen_depth_toggle(opts, prompt_bufnr))
+        return true
+      end,
+    })
+    :find()
 end
