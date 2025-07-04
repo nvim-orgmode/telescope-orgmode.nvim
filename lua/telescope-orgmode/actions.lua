nvim-orgmode/telescope-orgmode.nvim

commit 6a53595cb96c49bf605ebf1ffa56d7ec3554361e

perf: reduce org file loading time

lua/telescope-orgmode/org.lua
@@ -3,15 +3,14 @@ local OrgApi = require('orgmode.api')
 local M = {}
 
 function M.load_files(opts)
-  ---@type { file: OrgApiFile, filename: string, last_used: number }[]
+  ---@type { filename: string, last_used: number, title: string }[]
   local file_results = vim.tbl_map(function(file)
-    local file_stat = vim.uv.fs_stat(file.filename) or 0
-    return { file = file, filename = file.filename, last_used = file_stat.mtime.sec }
-  end, OrgApi.load())
+    return { filename = file.filename, last_used = file.metadata.mtime, title = file:get_title()   }
+  end, require('orgmode').files:all())
 
   if not opts.archived then
     file_results = vim.tbl_filter(function(entry)
-      return not entry.file.is_archive_file
+      return not (vim.fn.fnamemodify(entry.filename, ':e') == 'org_archive')
     end, file_results)
   end
 

lua/telescope-orgmode/entry_maker/orgfiles.lua
@@ -4,17 +4,14 @@ local entry_display = require('telescope.pickers.entry_display')
 
 local M = {}
 
----@param file_results { file: OrgApiFile, filename: string }[]
+---@param file_results { filename: string, title: string, headline: string }[]
 ---@return OrgFileEntry[]
 local function index_orgfiles(file_results)
   local results = {}
   for _, file_entry in ipairs(file_results) do
     local entry = {
-      file = file_entry.file,
       filename = file_entry.filename,
-      -- not beautiful to access a private property, but this is the only way to get the title
-      ---@diagnostic disable-next-line: invisible, undefined-field
-      title = file_entry.file._file:get_directive('TITLE') or nil,
+      title = file_entry.title,
       headline = nil,
     }
     table.insert(results, entry)

commit 366f68d0fceacfdf100740a45ec283bfce7a75b4

fix: adjust refile and link feature to api changes
Add helper methods to retrieve OrgApi objects from picker entries and
use them in actions.

Fixes bug introduced by 6a53595 and 4ee4502.

lua/telescope-orgmode/actions.lua
@@ -50,12 +50,12 @@ function M.refile(closest_headline)
     local entry = action_state.get_selected_entry()
     actions.close(prompt_bufnr)
 
-    -- Refile to the file by default
-    local destination = entry.value.file
+    local destination = entry.value.headline
+      and org.get_api_headline(entry.filename, entry.value.headline.line_number)
+      or org.get_api_file(entry.filename)
 
-    -- Refile to a specific heading if is set
-    if entry.value.headline then
-      destination = entry.value.headline
+    if not destination then
+      error('Could not find destination headline or file')
     end
 
     return org.refile({
@@ -72,14 +72,15 @@ function M.insert(_)
     ---@type MatchEntry
     local entry = action_state.get_selected_entry()
 
-    local destination = (function()
-      if entry.value.headline then
-        -- Link to a specific heading if is set
-        return entry.value.headline:get_link()
-      else
-        return entry.value.file:get_link()
-      end
-    end)()
+    local api_object = entry.value.headline
+      and org.get_api_headline(entry.filename, entry.value.headline.line_number)
+      or org.get_api_file(entry.filename)
+    
+    if not api_object then
+      error('Could not find ' .. (entry.value.headline and 'headline' or 'file') .. ' for link')
+    end
+    
+    local destination = api_object:get_link()
 
     org.insert_link(destination)
     return true

lua/telescope-orgmode/org.lua
@@ -95,4 +95,27 @@ function M.get_closest_headline()
   return OrgApi.current():get_closest_headline()
 end
 
+--- Get the API headline object for a given filename and line number
+---@param filename string
+---@param line_number number
+---@return table|nil
+function M.get_api_headline(filename, line_number)
+  local api_file = OrgApi.load(filename)
+  if api_file then
+    for _, headline in ipairs(api_file.headlines) do
+      if headline.position.start_line == line_number then
+        return headline
+      end
+    end
+  end
+  return nil
+end
+
+--- Get the API file object for a given filename
+---@param filename string
+---@return table|nil
+function M.get_api_file(filename)
+  return OrgApi.load(filename)
+end
+
 return M