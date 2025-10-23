e3bcb36b422de4200bd7e962d45c1b5fb26179ec
Sebastian Flügge <sebastian.fluegge@dnv.com>
2025-10-23 13:35:33 +0200

    feat: advanced filtering - implement keybindings

--- lua/telescope-orgmode/lib/keybindings.lua
+++ lua/telescope-orgmode/lib/keybindings.lua
@@ -28,6 +28,22 @@ M.bindings = {
     description = 'Return to headlines picker from tags',
     modes = { i = '<C-t>', n = '<C-t>' },
   },
+  filter_current_buffer = {
+    description = 'Filter to current buffer',
+    modes = { i = '<C-f><C-b>', n = '<C-f><C-b>' },
+  },
+  filter_all_buffers = {
+    description = 'Filter to all open buffers',
+    modes = { i = '<C-f><C-a>', n = '<C-f><C-a>' },
+  },
+  filter_headline_file = {
+    description = 'Filter to selected headline file',
+    modes = { i = '<C-f><C-h>', n = '<C-f><C-h>' },
+  },
+  drop_filters = {
+    description = 'Clear all filters',
+    modes = { i = '<C-f><C-d>', n = '<C-f><C-d>' },
+  },
 }
 
 ---Execute keybinding action (framework-agnostic logic)
@@ -59,6 +75,31 @@ function M.execute_action(action_name, context)
       default_text = selected_tag,
       context = context.opts.context,
     })
+  elseif action_name == 'filter_current_buffer' then
+    local current_file = context.opts.current_file
+    context.state:set_filter('current_files', { current_file })
+    context.refresh_fn(context.state)
+  elseif action_name == 'filter_all_buffers' then
+    local filters = require('telescope-orgmode.lib.filters')
+    local open_buffers = filters.get_open_buffers()
+    context.state:set_filter('current_files', open_buffers)
+    context.refresh_fn(context.state)
+  elseif action_name == 'filter_headline_file' then
+    if context.state:get_current() ~= 'headlines' then
+      return
+    end
+    if not context.selected_entry then
+      return
+    end
+
+    local headline_file = context.selected_entry.filename
+    context.state:set_filter('current_files', { headline_file })
+    context.refresh_fn(context.state)
+  elseif action_name == 'drop_filters' then
+    context.state:set_filter('tag_query', nil)
+    context.state:set_filter('current_files', {})
+    context.state:set_filter('only_current_file', nil)
+    context.refresh_fn(context.state)
   end
 end
 
